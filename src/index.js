43224731e2d3e94b11474609f561e9ecde14bd01
Oybek <oybekjonxayrullayevich@gmail.com>
2022-04-22 08:59:48 +0500

    style plus

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import DisplaySeason from "./DisplaySeason";
+// Functional component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const App = ()=>{
+//   return <div>Hello World</div>
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { latitude: null };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        {
+          this.setState({
+            latitude: position.coords.latitude,
+          });
+        }
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  getLocation() {
+    if (this.state.latitude) {
+      return <div>Latitude:{this.state.latitude}</div>;
+    } else if (this.state.errorMessage) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    } else if (this.state.latitude && this.state.errorMessage) {
+      return <div>loading...</div>;
+    }
+  }
+  render() {
+    return (
+      <DisplaySeason lat={this.state.latitude} err={this.state.errorMessage} />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
